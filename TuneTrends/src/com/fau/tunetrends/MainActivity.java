aab463395cf35c25581710fb23f6d762cfc32a37
Mike <mramos28>
2014-04-01 21:08:41 -0400

    First Commit of project

---
+++ TuneTrends/src/com/fau/tunetrends/MainActivity.java
@@ -0,0 +1,37 @@
+package com.fau.tunetrends;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button submitBtn = (Button) findViewById(R.id.submitBtn);
+		submitBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(getApplicationContext(), MenuScreen.class);
+				startActivity(i);
+				finish();
+			}
+		});
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
