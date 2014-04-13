c1ff827c644e02d42f488de2c899aed60a288705
Mike <mramos28>
2014-04-12 21:02:56 -0400

    Began making songs list

--- TuneTrends/src/com/fau/tunetrends/MenuScreen.java
+++ TuneTrends/src/com/fau/tunetrends/MenuScreen.java
@@ -1,20 +1,40 @@
 package com.fau.tunetrends;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.ListActivity;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MenuScreen extends ListActivity {
+public class MenuScreen extends Activity {
 	
 	// This is the Adapter being used to display the list's data
-    SimpleCursorAdapter mAdapter;
+    private ListView lv;
+   
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_menu_screen);
+		
+		lv = (ListView) findViewById(R.id.trackList);
+		
+		List<String> songsList = new ArrayList<String>();
+		songsList.add("foo");
+		songsList.add("bar");
+		
+		// This is the array adapter, it takes the context of the activity as a 
+        // first parameter, the type of list view as a second parameter and your 
+        // array as a third parameter.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this, 
+                android.R.layout.simple_list_item_1,
+                songsList );
+
+        lv.setAdapter(arrayAdapter); 
 	}
 
 	@Override
