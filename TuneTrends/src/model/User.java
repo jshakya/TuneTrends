a14b24436b85dfd8b01ae6ea7c9cd41ed55a9a31
Jebin Shakya <jebinshakya@gmail.com>
2014-04-13 19:03:34 -0400

    Strings fixed User fields like email/username and password added Layout of Register created Updated main activity view

--- TuneTrends/src/model/User.java
+++ TuneTrends/src/model/User.java
@@ -105,10 +105,27 @@ public class User {
 		this.group = group;
 	}
 	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	private String fName;
 	private String lName;
 	private UserGroup group;
 	public static User currentUser;
-	
+	private String email;
+	private String password;
 
 }
