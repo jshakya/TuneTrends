59cf5e11be3a5cdb0ab05bf03f31116c37ede253
Jebin Shakya <jebinshakya@gmail.com>
2014-04-20 19:06:16 -0400

    Setting RatingsValue fixed and working. Note: need to work on back to prevScreen Intent

--- TuneTrends/src/com/fau/tunetrends/UpdateRating.java
+++ TuneTrends/src/com/fau/tunetrends/UpdateRating.java
@@ -1,16 +1,27 @@
 package com.fau.tunetrends;
 
+import model.UserGroup;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class UpdateRating extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
+		
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_update_rating);
+		final Button upBtn= (Button) findViewById(R.id.rateUpBtn);
+		final Button downBtn = (Button) findViewById(R.id.rateDownBtn);
+		final TextView ratingTxt = (TextView) findViewById(R.id.ratingValueTxt);
+		
 		int position = 0;
 		
 		final Bundle extras = getIntent().getExtras();
@@ -25,6 +36,28 @@ public class UpdateRating extends Activity {
 			Toast.makeText(getApplicationContext(),
 					"error", Toast.LENGTH_SHORT).show();
 		}
+		final int pos = position;
+		//Setting the textView
+		ratingTxt.setText(String.valueOf(UserGroup.getTrackList().getTrackList().get(pos).getRatingValue()));
+		upBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				UserGroup.getTrackList().getTrackList().get(pos).upRateValue();
+				ratingTxt.setText(String.valueOf(UserGroup.getTrackList().getTrackList().get(pos).getRatingValue()));
+			}
+		
+		});
+		downBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				UserGroup.getTrackList().getTrackList().get(pos).downRateValue();
+				ratingTxt.setText(String.valueOf(UserGroup.getTrackList().getTrackList().get(pos).getRatingValue()));
+			}
+		
+		});
+		
 	}
 
 	@Override
