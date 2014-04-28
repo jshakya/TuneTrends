b9a6476086db49df83f7947fd174cc95e92d7d37
Jebin Shakya <jebinshakya@gmail.com>
2014-04-27 20:13:54 -0400

    giterror test

--- TuneTrends/src/com/fau/tunetrends/MainActivity.java
+++ TuneTrends/src/com/fau/tunetrends/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //JEBIN GITTEST
         if (curUserGroup == null) {
             curUserGroup = new UserGroup();
             this.loadFile();
